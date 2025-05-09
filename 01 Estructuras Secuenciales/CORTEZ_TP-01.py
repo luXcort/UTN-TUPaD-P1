065f8c89b47ec6c60d9f05b29a58956edd0df6a4
lucas <cortezlucas10@gmail.com>
2025-05-09 02:50:16 -0300

    primer trabajo practico

---
+++ 01 Estructuras Secuenciales/CORTEZ_TP-01.py
@@ -0,0 +1,93 @@
+#Ejercicio 1
+
+print("hola mundo")
+
+#Ejercicio 2
+"""
+nombre=input("ingrese su nombre= ")
+print(f"hola {nombre}")
+"""
+#Ejercicio 3
+""""
+nombre=input("ingrese su nombre= ")
+apellido=input("ingrese su apellido= ")
+edad=input("ingrese su edad= ")
+pais=input("ingrese su pais de residencia= ")
+
+print(f"Soy {nombre} {apellido}, tengo {edad} y vivo en {pais}")
+"""
+#Ejercicio 4
+"""
+radio=int(input("ingrese el radio del circulo "))
+area=3.14*radio**2
+print("El area del circulo es: ",area)
+"""
+
+#Ejercicio 5
+"""
+segundos=int(input("ingrese una cantidad de segundos "))
+hora=int((segundos/60)/60)
+print("la cantidad de horas es: ",hora)
+"""
+
+#Ejercicio 6
+""""
+
+tabla=int(input("ingrese el numero a calcular"))
+print(f"la tabla del {tabla} es ")
+print(tabla*1)
+print(tabla*2)
+print(tabla*3)
+print(tabla*4)
+print(tabla*5)
+print(tabla*6)
+print(tabla*7)
+print(tabla*8)
+print(tabla*9)
+print(tabla*10)
+
+"""
+
+#Ejercicio 7
+"""
+num1=int(input("ingrese el primer numero "))
+num2=int(input("ingrese el segundo numero "))
+
+suma=num1+num2
+div=num1/num2
+mult=num1*num2
+rest=num1-num2
+
+print(f"la suma de los numero es {suma}")
+print(f"la division de los numero es {div}")
+print(f"la multiplicacion de los numero es {mult}")
+print(f"la resta de los numero es {rest}")"
+
+"""
+#Ejercicio 8
+
+"""
+altura=int(input("ingrese su altura "))
+peso=int(input("ingrese su peso "))
+imc=(peso/(altura*100)**2)
+print("su indice de masa corporal: ",imc)
+
+"""
+
+#Ejercicio 9
+"""
+temp=int(input("ingrese la temperatura a convertir "))
+Fahr= (temp*9/5)+32
+print(f"{temp} celcius es en fahrenheit {Fahr}")
+
+"""
+
+#Ejercicio 10
+"""
+
+num1=int(input("ingrese el primer numero "))
+num2=int(input("ingrese el segundo numero "))
+num3=int(input("ingrese el tercer numero "))
+prom=(num1+num2+num3)/3
+
+"""
